8843923bb2c7b4321e71035488e2f723b4fb41c5
Kevin Wittek <kevin.wittek@styracosoft.de>
2018-11-03 15:37:13 +0100

    Some scripts for batch plotting

--- example_plot.py
+++ example_plot.py
@@ -1,11 +1,11 @@
+import glob
 import json
-import os
 
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, transforms, pyplot
 from matplotlib.colors import LogNorm
 
 
-def plot_json(input_file, output_name):
+def plot_json(input_file, output_name, show=False):
     with open(input_file, ) as f:
         result_json = f.read()
     result = json.loads(result_json)
@@ -14,17 +14,33 @@ def plot_json(input_file, output_name):
     for point in result['movements']:
         x.append(point[0])
         y.append(point[1])
+
+    axes = plt.gca()
+    axes.set_xlim([500, 1300])
+    axes.set_ylim([0, 1000])
+
+    # opencv coordinate systems is turned by 180° degree
+    # not working though? :(
+    # base_transformation = pyplot.gca().transData
+    # rotation = transforms.Affine2D().rotate_deg(180)
+    # transformation = base_transformation + rotation
+
     plt.plot(x, y)
     plt.savefig(output_name + '.plot.png')
     plt.hist2d(x, y, 20, norm=LogNorm())
     plt.colorbar()
     plt.savefig(output_name + '.histo.png')
-    plt.show()
+
+    if show:
+        plt.show()
+    else:
+        plt.clf()
 
 
-json_dir = "/home/kiview/Development/Pigeons/pigeon-tracker/"
-json_in_dir = [x for x in os.listdir(json_dir) if x.endswith(".json")]
+json_files = glob.glob("results/**/*.json", recursive=True)
 
-for json_file in json_in_dir:
-    plot_json(json_dir + json_file, json_file)
+for json_file in json_files:
+    print("Plotting file " + json_file)
+    plot_name = json_file.replace(".json", "")
+    plot_json(json_file, json_file)
 
